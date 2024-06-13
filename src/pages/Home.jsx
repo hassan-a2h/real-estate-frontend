1e8fd9de22c427dc6272c6b7440390d20bf6ab10
hassan <asd@MacBook-Air-von-asd.local>
2024-06-13 17:21:54 +0500

    stylize the Homepage component using custom css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <>
+    <div className="login-form homepage">
       <div className="flex items-center justify-center h-32 bg-gray-100">
         <h2 className="text-2xl font-bold">Welcome to the Home Page</h2>
-      </div>
+      </div> <br />
       <div className='flex justify-center'>
-        <Link to='/listings' className='m-4 p-4 px-8 bg-green-500 rounded-lg shadow-2xl'>Property Listings</Link>
+        <Link to='/listings' className='link'>Property Listings</Link>
       </div>
-    </>
+    </div>
   );
 };
 

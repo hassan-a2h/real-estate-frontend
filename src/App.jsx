63958a0f3fbf6ac446f88612cac0af021fbc3ffe
hassan <asd@MacBook-Air-von-asd.local>
2024-06-14 19:32:54 +0500

    create separate component for showing property listings by category and by agent

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Register from './pages/Register';
 import Home from './pages/Home';
 import Navbar from './components/UpdatedNavbar';
 import Listings from './pages/Listings';
+import CustomListings from './pages/CustomListings';
 import ListingForm from './components/ListingForm';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -24,6 +25,7 @@ function App() {
             <Route path="/listings" element={<Listings />} />
             <Route path="/listings/new" element={<ListingForm />} />
             <Route path="/listings/edit/:id" element={<ListingForm editing />} />
+            <Route path="/listings/:type/:value" element={<CustomListings />} />
           </Route>
         </Routes>
       </AuthProvider>
---
+++ src/pages/CustomListings.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import Listing from '../components/Listing';
+
+function CustomListings() {
+  const [filteredListings, setFilteredListings] = useState([]);
+  const { type, value } = useParams();
+  
+  useEffect(() => {
+    fetchListings(value);
+  }, [ value ]);
+
+  const fetchListings = async (value) => {
+    try {
+      let response;
+      if (type === 'agent') {
+        response = await axios.get('/api/listings', {
+          params: { agentId: value }
+        });  
+      } else {
+        response = await axios.get('/api/listings', {
+          params: { category: value }
+        });
+      }
+
+      setFilteredListings(response.data);
+    } catch (error) {
+      console.error('Error fetching listings:', error);
+      toast.error('Error fetching listings.');
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`/api/listings/${id}`);
+      fetchListings();
+      toast.success('Listing deleted successfully.');
+    } catch (error) {
+      console.error('Error deleting listing:', error);
+      toast.error('Error deleting listing.');
+    }
+  };
+
+  return (
+    <Listing listings={filteredListings} handleDelete={handleDelete}/>
+  );
+}
+
+export default CustomListings;

aed0abbb9ab78b6934832e79bb993e954d34110e
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-06-27 20:34:39 +0500

    create utility function for formatting dateTime to local time using moment library

--- src/utils/localDateTime.js
+++ src/utils/localDateTime.js
@@ -1,16 +1,41 @@
 import moment from 'moment-timezone';
 
-function convertToKarachiTime(dateString) {
-  // 1. Parse the date string into a moment object (in UTC)
+function convertToKarachiTime(dateString, options = {}) {
   const utcMoment = moment.utc(dateString);
 
-  // 2. Convert to Karachi time zone
   const karachiMoment = utcMoment.tz("Asia/Karachi");
-
-  // 3. Format the local date according to your desired format
   const formattedKarachiTime = karachiMoment.format();
 
-  return formattedKarachiTime;
+  console.log('karachi formatted time:', formattedKarachiTime);
+  
+  if (options?.onlyHours) {
+    return formattedKarachiTime?.split('T')[1].slice(0, 5).replace('.', ':');
+  }
+
+  const currentDate = new Date();
+
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1; // Months are zero-indexed
+  const currentDay = currentDate.getDate();
+
+  let [ messageYear, messageMonth, messageDay ] = formattedKarachiTime.split('T')[0].split('-'); 
+  let finalDate = '';
+
+  if (currentYear != messageYear) {
+    finalDate += Number(currentYear) - Number(messageYear);
+    const unit = +finalDate > 1 ? 'years' : 'year';
+    return finalDate + ` ${unit} ago`;
+  } else if (currentMonth != messageMonth) {
+    finalDate += Number(currentMonth) - Number(messageMonth);
+    const unit = +finalDate > 1 ? 'months' : 'year';
+    return finalDate + ` ${unit} ago`;
+  } else if (currentDay != messageDay) {
+    finalDate += Number(currentDay) - Number(messageDay);
+    const unit = +finalDate > 1 ? 'days' : 'day';
+    return finalDate + ` ${unit} ago`;
+  }
+
+  return formattedKarachiTime?.split('T')[1].slice(0, 5).replace('.', ':');
 }
 
 export default convertToKarachiTime;

fa629c6bf6608f8d8b168d864dbfd18f507f19ff
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-01 17:03:50 +0500

    implement pagination for messages

--- src/pages/Chat/ChatContent.jsx
+++ src/pages/Chat/ChatContent.jsx
@@ -9,6 +9,8 @@ const socket = io('http://localhost:3000');
 const ChatContent = ({ currentChat, userId, socket }) => {
   const [messages, setMessages] = useState([]);
   const [titleMessages, setTitleMessages] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
   const id = localStorage.getItem('userId');
   const messagesEndRef = useRef(null);
   const currentChatRef = useRef(currentChat);
@@ -27,6 +29,8 @@ const ChatContent = ({ currentChat, userId, socket }) => {
 
   useEffect(() => {
     if (currentChat) {
+      setMessages([]);
+      setHasMore(true);
       fetchMessages();
     }
   }, [currentChat]);
@@ -49,15 +53,33 @@ const ChatContent = ({ currentChat, userId, socket }) => {
     }
   }
 
-  const fetchMessages = async () => {
+  const fetchMessages = async (lastMessageDate = null) => {
+    if (isLoading || !hasMore) return;
+
+    setIsLoading(true);
     try {
       const response = await axios.get(`/api/c/chats/${currentChat._id}/messages`, {
-        params: { userId },
+        params: { 
+          userId,
+          lastMessageDate,
+          limit: 20
+        },
       });
-      setMessages(response.data);
+      const newMessages = response.data;
+      setMessages((prevMessages) => [...prevMessages, ...newMessages]);
+      setHasMore(newMessages.length === 20);
       socket.emit('messagesRead', { userId });
     } catch (error) {
       console.error('Error fetching messages:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const loadMoreMessages = () => {
+    if (messages.length > 0) {
+      const oldestMessage = messages[messages.length - 1];
+      fetchMessages(oldestMessage.createdAt);
     }
   };
 
@@ -97,6 +119,9 @@ const ChatContent = ({ currentChat, userId, socket }) => {
             titleMessages={titleMessages}
             fetchTitleMessages={fetchTitleMessages}
             messagesEndRef={messagesEndRef}
+            loadMoreMessages={loadMoreMessages}
+            isLoading={isLoading}
+            hasMore={hasMore}
           />
           <MessageForm sendMessage={sendMessage} />
         </>
--- src/pages/Chat/MessageList.jsx
+++ src/pages/Chat/MessageList.jsx
@@ -1,45 +1,77 @@
-import React from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import localTime from '../../utils/localDateTime.js';
 
-const MessageList = ({ messages, userId, titleMessages, fetchTitleMessages }) => {
-  return (
-    <div className="chat-history" style={{ height: '80%', overflowY: 'scroll' }}>
-      {messages.map((msg, index) => {
-        if (msg.isPropertyTitle) {
-          if (!titleMessages?.[msg.message]) {
-            fetchTitleMessages(msg.message);
-          }
+const MessageList = ({ messages, userId, titleMessages, fetchTitleMessages, messagesEndRef, loadMoreMessages, isLoading, hasMore }) => {
+  const listRef = useRef(null);
+  const observerRef = useRef(null);
 
-          return (
-            <React.Fragment key={index}>
-              {titleMessages[msg.message] !== undefined ? (
-                <div className="property-details">
-                  <h2 className="property-title">{titleMessages[msg.message].title}</h2>
-                  <p className="property-description">{titleMessages[msg.message].description}</p>
-                  <div className="property-info">
-                    <span className="property-price">{titleMessages[msg.message].price}</span> - 
-                    <span className="property-location">{titleMessages[msg.message].location}</span>
-                  </div>
-                </div>
-              ) : (
-                <div className="property-details placeholder">
-                  <h2>Loading...</h2>
-                  <p>&nbsp;</p>
-                  <div className="property-info">
-                    <span>&nbsp;</span> - <span>&nbsp;</span>
-                  </div>
-                </div>
-              )}
-            </React.Fragment>
-          );
+  const topMessageRef = useCallback(node => {
+    if (isLoading) return;
+    if (observerRef.current) observerRef.current.disconnect();
+    observerRef.current = new IntersectionObserver(entries => {
+      if (entries[entries.length - 1].isIntersecting && hasMore) {
+        if (entries[entries.length - 1].isVisible) {
+          loadMoreMessages();
         }
-        return (
-          <div key={index} className={`chat-message ${msg.senderId === userId ? 'chat-message-sent' : 'chat-message-received'}`}>
-            {msg.message}
-            <span className="message-timestamp">{localTime(msg.createdAt)}</span>
+      }
+    });
+    if (node) observerRef.current.observe(node);
+  }, [isLoading, hasMore, loadMoreMessages]);
+
+  return (
+    <div ref={listRef} className='chat-history-wrapper' style={{ height: '80%', overflowY: 'scroll' }}>
+      {isLoading && <div>Loading...</div>}
+      <div className="chat-history">
+        {messages.map((msg, index) => {
+          if (index === messages.length - 1) {
+            {console.log('messge index:', index, 'messages length:', messages.length - 1)}
+            return (
+              <div ref={topMessageRef} key={msg._id || index}>
+                {renderMessage(msg, index, userId, titleMessages, fetchTitleMessages)}
+              </div>
+            );
+          }
+          return renderMessage(msg, index, userId, titleMessages, fetchTitleMessages);
+        })}
+        <div ref={messagesEndRef} />
+      </div>
+    </div>
+  );
+};
+
+const renderMessage = (msg, index, userId, titleMessages, fetchTitleMessages) => {
+  if (msg.isPropertyTitle) {
+    if (!titleMessages?.[msg.message]) {
+      fetchTitleMessages(msg.message);
+    }
+
+    return (
+      <React.Fragment key={index}>
+        {titleMessages[msg.message] !== undefined ? (
+          <div className="property-details">
+            <h2 className="property-title">{titleMessages[msg.message].title}</h2>
+            <p className="property-description">{titleMessages[msg.message].description}</p>
+            <div className="property-info">
+              <span className="property-price">{titleMessages[msg.message].price}</span> - 
+              <span className="property-location">{titleMessages[msg.message].location}</span>
+            </div>
           </div>
-        );
-      })}
+        ) : (
+          <div className="property-details placeholder">
+            <h2>Loading...</h2>
+            <p>&nbsp;</p>
+            <div className="property-info">
+              <span>&nbsp;</span> - <span>&nbsp;</span>
+            </div>
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
+  return (
+    <div key={msg._id || index} className={`chat-message ${msg.senderId === userId ? 'chat-message-sent' : 'chat-message-received'}`}>
+      {msg.message}
+      <span className="message-timestamp">{localTime(msg.createdAt)}</span>
     </div>
   );
 };
